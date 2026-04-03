49723e0f351cd4fff58a97f76554a7a6157a4557
= <=>
2026-04-03 10:52:04 +0700

    test crud

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,24 +12,31 @@
 
 
     <h1>Data User</h1>
+    <a href="{{ url('/user/tambah') }}">+ Tambah User</a>
+    <br>
+    <br>
+
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>Level Pengguna</th>
-            {{-- <th>Jumlah Pengguna</th> --}}
+            <th>Aksi</th>
         </tr>
 
-        <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
-            {{-- <td>{{ $data }}</td> --}}
-        </tr>
+        @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td><a href="{{ url('user/ubah', $d->user_id) }}">ubah</a> | <a
+                        href="{{ url('user/hapus', $d->user_id) }}">Hapus</a>
+                </td>
+            </tr>
+        @endforeach
     </table>
-
 </body>
 
 </html>
---
+++ resources/views/user_tambah.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tambah User</title>
+</head>
+
+<body>
+
+    <h1>Form Tambah Data User</h1>
+
+    <form action="{{ url('user/tambah_simpan') }}" method="post">
+        {{ csrf_field() }}
+
+        <label for="">Username</label>
+        <input type="text" name="username" placeholder="Masukan Username">
+        <br>
+
+        <label for="">Nama</label>
+        <input type="text" name="nama" placeholder="Masukan Nama">
+        <br>
+
+        <label for="">Password</label>
+        <input type="password" name="password" placeholder="Masukan Password">
+        <br>
+
+        <label for="">Level ID</label>
+        <input type="number" name="level_id" placeholder="Masukan ID Level">
+        <br><br>
+
+        <input type="submit" class="btn btn-success" value="Tambah">
+    </form>
+
+</body>
+
+</html>
