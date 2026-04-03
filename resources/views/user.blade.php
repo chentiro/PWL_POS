7ed963f5a8d28801045d613a60335c45479c3730
= <=>
2026-04-03 13:47:45 +0700

    test relationship

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -21,7 +21,9 @@
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>Level Pengguna</th>
+            <th>ID Level Pengguna</th>
+            <th>Kode Level</th>
+            <th>Nama Level</th>
             <th>Aksi</th>
         </tr>
 
@@ -31,6 +33,8 @@
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->level_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
                 <td><a href="{{ url('user/ubah', $d->user_id) }}">ubah</a> | <a
                         href="{{ url('user/hapus', $d->user_id) }}">Hapus</a>
                 </td>
